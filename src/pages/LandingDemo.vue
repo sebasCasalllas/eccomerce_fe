c8617dd7646314cade5c9cb906570284d8e354d1
SebasCasallas <secasval96@gmail.com>
2022-08-22 19:58:52 -0500

    fix front

---
+++ src/pages/LandingDemo.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="surface-0 overflow-hidden">
+        <div class="py-4 px-4 mx-0 md:mx-6 lg:mx-8 lg:px-8 flex align-items-center justify-content-between relative lg:static">
+            <router-link to="/" class="flex align-items-center">
+                <img src="layout/images/LogAgro.jpg" alt="Sakai Logo" height="50" class="mr-0 lg:mr-2"><span class="text-900 font-medium text-2xl line-height-3 mr-8">ECCOMERCE</span>
+            </router-link>
+            <a class="cursor-pointer block lg:hidden text-700 p-ripple" v-ripple
+                v-styleclass="{ selector: '@next', enterClass: 'hidden', leaveToClass: 'hidden', hideOnOutsideClick: true }">
+                <i class="pi pi-bars text-4xl"></i>
+            </a>
+            <div class="surface-0 align-items-center flex-end-1 justify-content-end hidden lg:flex absolute lg:static w-full left-0 px-6 lg:px-30 z-2" style="top:92%">
+                <div class="flex justify-content-between lg:block border-top-1 lg:border-top-none surface-border py-3 lg:py-0 mt-3 lg:mt-0">
+                    <Button label="Inicio" class="p-button-text p-button-rounded border-none font-light line-height-2 text-blue-500"></Button>
+                    <Button label="Registrarse" class="p-button-rounded border-none ml-5 font-light text-white line-height-2 bg-blue-500"></Button>
+                </div>
+            </div>
+        </div>
+
+        <div class="grid py-4 px-4 lg:px-8 relative" id="hero">
+            <div class="mx-4 md:mx-8 mt-0 md:mt-4">
+                <h1 class="text-6xl font-bold text-gray-900 line-height-2"><span class="font-light block">Bienvenido a ECCOMERCE</span>Tienda virtual de productos del Agro</h1>
+                <p class="font-normal text-2xl line-height-3 md:mt-3 text-gray-700">En esta tienda virtual encontra difernetes productos del Agro, como lo son fumigadoras, fertilizantes herramientas ... </p>
+                <Button label="Ingresa" class="p-button-rounded text-xl border-none mt-5 bg-blue-500 font-normal text-white line-height-3 px-3"></Button>
+            </div>
+            <img src="layout/images/ladingFondo.jpg" class="bottom-0" height="350" alt="hero screen" style="right:10%; opacity: 0.8;">
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        smoothScroll(id){
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    },
+    computed: {
+        logoColor() {
+            if (this.$appState.darkTheme) return 'white';
+            return 'dark';
+        }
+    }
+}
+</script>
+
+<style>
+#hero{
+    background: linear-gradient(0deg, rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2)), radial-gradient(77.36% 256.97% at 77.36% 57.52%, #EEEFAF 0%, #C3E3FA 100%);
+    height:700px;
+    overflow:hidden;
+}
+
+@media screen and (min-width: 768px) {
+    #hero{
+        -webkit-clip-path: ellipse(150% 87% at 93% 13%);
+        clip-path: ellipse(150% 87% at 93% 13%);
+        height: 530px;
+    }
+}
+
+@media screen and (min-width: 1300px){
+    #hero > img {
+        position: absolute;
+    }
+
+    #hero > div > p { 
+        max-width: 450px;
+    }
+}
+
+@media screen and (max-width: 1300px){
+    #hero {
+        height: 600px;
+    }
+
+    #hero > img {
+        position:static;
+        transform: scale(1);
+        margin-left: auto;
+    }
+
+    #hero > div {
+        width: 100%;
+    }
+
+    #hero > div > p {
+        width: 100%;
+        max-width: 100%;
+    }
+}
+</style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="surface-0 flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
+        <div class="grid justify-content-center p-2 lg:p-0" style="min-width:80%">
+            <div class="col-12 mt-5 xl:mt-0 text-center">
+                <img src="layout/images/agro.jpg" class="mb-5" style="width:120px; height:150px;">
+            </div>
+            <br>
+            <br>
+            <div class="col-12 xl:col-6" style="border-radius:56px; padding:0.3rem; background: linear-gradient(180deg, var(--primary-color), rgba(33, 150, 243, 0) 30%);">
+                <div class="h-full w-full m-0 py-7 px-4" style="border-radius:53px; background: linear-gradient(180deg, var(--surface-50) 38.9%, var(--surface-0));">
+                    <div class="text-center mb-5">
+                        <div class="text-900 text-3xl font-medium mb-3">Bienvenido!</div>
+                        <span class="text-600 font-medium">Ingrese para continuar</span>
+                    </div>
+                
+                    <div class="w-full md:w-10 mx-auto">
+                        <label for="email1" class="block text-900 text-xl font-medium mb-2">Username</label>
+                        <InputText id="email1" v-model="email" type="text" class="w-full mb-3" placeholder="Username" style="padding:1rem;" />
+                
+                        <label for="password1" class="block text-900 font-medium text-xl mb-2">Password</label>
+                        <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true" class="w-full mb-3" inputClass="w-full" inputStyle="padding:1rem"></Password>
+                
+                        <Button label="Sign In" class="w-full p-3 text-xl"></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            checked: false
+        }
+    },
+    computed: {
+        logoColor() {
+            if (this.$appState.darkTheme) return 'white';
+            return 'dark';
+        }
+    }
+}
+</script>
+
+<style scoped>
+.pi-eye {
+    transform:scale(1.6);
+    margin-right: 1rem;
+}
+
+.pi-eye-slash {
+    transform:scale(1.6);
+    margin-right: 1rem;
+}
+
+.Img img {
+    height: 200px;
+    display: block;
+    border-radius: 200px;
+    border: 1px solid white;
+    margin: 100px auto 20px auto;
+}
+</style>
